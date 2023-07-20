86cd66eccf8683baeaa95dd08c248f00f384b936
qiuxan <qiuxan@gmail.com>
2023-07-20 17:37:47 +1000

    5- Templates

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'courses',
-  template: '<h2>Courses</h2>',
+  template: '<h2>{{"Title: "+getTitle()}}</h2>',
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+  title = 'List of courses';
+  getTitle() {
+    return this.title;
+  }
+}
