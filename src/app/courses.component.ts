141ce5af19c5f6645acb3ed2a9ad4d5be2158acd
qiuxan <qiuxan@gmail.com>
2023-07-21 18:52:44 +1000

    11- Pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,6 +3,11 @@ import { CoursesService } from './courses.service';
 @Component({
   selector: 'courses',
   template: `
+    {{ course.title | uppercase }}<br />
+    {{ course.rating | number : '2.2-2' }}<br />
+    {{ course.student | number : '1.2-2' }}<br />
+    {{ course.price | currency : 'AUD' : false : '2.2-2' }}<br />
+    {{ course.releaseDate | date : 'shortDate' }}
     <!-- <button class="btn btn-primary" [class.active]="isActive">Save</button> -->
     <!-- <button [style.background]="isActive ? 'blue' : 'white'">Save</button> -->
 
@@ -16,11 +21,18 @@ import { CoursesService } from './courses.service';
       (keyup.enter)="email = checkEvent($event); onKeyUp()"
     /> -->
 
-    <input [(ngModel)]="email" type="text" (keyup.enter)="onKeyUp()" />
+    <!-- <input [(ngModel)]="email" type="text" (keyup.enter)="onKeyUp()" /> -->
   `,
 })
 export class CoursesComponent {
-  email = 'iqiu@rm.com';
+  // email = 'iqiu@rm.com';
+  course = {
+    title: 'the complete angular course',
+    rating: 4.9073,
+    student: 30123,
+    price: 190.29,
+    releaseDate: new Date(2016, 3, 1),
+  };
 
   checkEvent($event: Event) {
     return ($event.target as HTMLInputElement).value;
@@ -31,9 +43,9 @@ export class CoursesComponent {
   //   console.log('Enter', ($event.target as HTMLInputElement).value);
   // }
 
-  onKeyUp() {
-    console.log(this.email);
-  }
+  // onKeyUp() {
+  //   console.log(this.email);
+  // }
   isActive = true;
   title = 'List of courses';
   courses;
