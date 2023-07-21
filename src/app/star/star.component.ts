619b464c797085fa92733d00417975494b6b5f5b
qiuxan <qiuxan@gmail.com>
2023-07-21 20:53:50 +1000

    13- Exercise- Favorite Component

---
+++ src/app/star/star.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css'],
+})
+export class StarComponent {
+  fullStar = false;
+  onClick() {
+    this.fullStar = !this.fullStar;
+  }
+}
