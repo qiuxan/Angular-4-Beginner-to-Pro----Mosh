27cee4435327f0b3360bc0fee7ede68965bf0eec
qiuxan <qiuxan@gmail.com>
2023-07-22 08:03:20 +1000

    first method

--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-star',
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./star.component.css'],
 })
 export class StarComponent {
-  fullStar = false;
+  @Input() isFavorite: boolean = true;
   onClick() {
-    this.fullStar = !this.fullStar;
+    this.isFavorite = !this.isFavorite;
   }
 }
