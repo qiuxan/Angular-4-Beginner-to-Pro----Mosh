85c77017b0f940020e37a8684cb3cdefbf6ed698
qiuxan <qiuxan@gmail.com>
2023-07-22 12:40:57 +1000

    go back to better way

--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css'],
-  inputs: ['isFavorite'],
 })
 export class StarComponent {
-  isFavorite: boolean = true;
+  @Input() isFavorite: boolean = true;
   onClick() {
     this.isFavorite = !this.isFavorite;
   }
